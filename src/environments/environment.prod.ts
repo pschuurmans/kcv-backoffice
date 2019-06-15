e3b26d41992e79da8fa0d6e200d6e5af61ad3166
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-06-15 11:44:29 +0200

    Add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCT1lAyZPEA3t1SosLRU6wLZ4qpU4RQQ6o',
+    authDomain: 'kcv-backoffice.firebaseapp.com',
+    databaseURL: 'https://kcv-backoffice.firebaseio.com',
+    projectId: 'kcv-backoffice',
+    storageBucket: 'kcv-backoffice.appspot.com',
+    messagingSenderId: '608735606795',
+    appId: '1:608735606795:web:ee24ff6cbb3fa73a'
+  }
 };
