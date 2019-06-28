c2683265819903621d4f5ada4c094791e5470d25
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-06-28 13:43:06 +0200

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,17 +10,20 @@ import { MyProfileComponent } from './modules/my-profile/containers/my-profile/m
 import { RegistrationsComponent } from './modules/registrations/containers/registrations/registrations.component';
 import { RegisterComponent } from './containers/register/register.component';
 import { LoginGuard } from './core/auth/login.guard';
+import { AccessGuard } from './core/auth/access.guard';
+import { NoAccessComponent } from './containers/no-access/no-access.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [LoginGuard] },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'items', component: ItemsComponent, canActivate: [AuthGuard] },
-  { path: 'registrations', component: RegistrationsComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard, AccessGuard] },
+  { path: 'items', component: ItemsComponent, canActivate: [AuthGuard, AccessGuard] },
+  { path: 'registrations', component: RegistrationsComponent, canActivate: [AuthGuard, AccessGuard] },
   { path: 'my-profile', component: MyProfileComponent, canActivate: [AuthGuard] },
 
+  { path: 'no-access', component: NoAccessComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' },
 ];
