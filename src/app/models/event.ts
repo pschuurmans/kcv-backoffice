56620fc503b0ff86923296cfb0caee155a8a54ad
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-07-29 17:31:45 +0200

    Add event and registration model

---
+++ src/app/models/event.ts
@@ -0,0 +1,6 @@
+export interface Event {
+    name: string;
+    year: string;
+    description: string;
+    tag: string;
+}
---
+++ src/app/models/registration.ts
@@ -0,0 +1,5 @@
+export interface Registration {
+    first_name: string;
+    last_name: string;
+    event_id: string;
+}
