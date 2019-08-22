7666f2d561e6cdd2b185df5c513004489e76fbfe
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-08-22 21:39:28 +0200

    Fix onCall functions with async code

--- functions/src/db/access/onCall.f.ts
+++ functions/src/db/access/onCall.f.ts
@@ -3,11 +3,11 @@ const admin = require('firebase-admin');
 try { admin.initializeApp(functions.config().firebase); } catch (e) { } // You do that because the admin SDK can only be initialized once.
 import { User } from "../../models/user";
 
-exports = module.exports = functions.https.onCall((data: User, context: any) => {
+exports = module.exports = functions.https.onCall(async (data: User, context: any) => {
     const doc = {
         roles: ['GUEST']
     }
 
-    console.log('Create doc with id ' + data.uid);
-    admin.firestore().collection('access').doc(data.uid).set(doc);
+    await admin.firestore().collection('access').doc(data.uid).set(doc);
+    console.log('Written data to doc with id ' + data.uid);
 });
