358ebff6aedabd8424355cdc0f2700598e8d7cca
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-08-25 14:18:56 +0200

    Fix lint

--- src/app/modules/events/component/event-script/event-script.component.ts
+++ src/app/modules/events/component/event-script/event-script.component.ts
@@ -21,7 +21,7 @@ export class EventScriptComponent implements OnInit {
       .subscribe(
         (data: any) => {
           if (data !== undefined) {
-            this.code = `/* 
+            this.code = `/*
   Copy and use in your own script, don't forget to use firestore library: 1VUSl4b1r1eoNcRWotZM3e87ygkxvXltOgyDZhixqncz9lQ3MjfT1iKFw
   If you need your own service account, please create at https://console.developers.google.com/projectselector/iam-admin/serviceaccounts
 
@@ -31,14 +31,14 @@ export class EventScriptComponent implements OnInit {
 function Initialize() {
   try {
     var triggers = ScriptApp.getProjectTriggers();
-    
+
     for (var i in triggers)
       ScriptApp.deleteTrigger(triggers[i]);
-    
+
     ScriptApp.newTrigger("SubmitGoogleFormData")
     .forSpreadsheet(SpreadsheetApp.getActiveSpreadsheet())
     .onFormSubmit().create();
-    
+
   } catch (error) {
     throw new Error("Please add this code in the Google Spreadsheet");
   }
@@ -48,8 +48,8 @@ function SubmitGoogleFormData(e) {
   if (!e) {
     throw new Error("Please go the Run menu and choose Initialize");
   }
-  
-  try {  
+
+  try {
     const data = createDataObject();
     createDocument(data);
     console.log(data);
@@ -61,37 +61,37 @@ function SubmitGoogleFormData(e) {
 function getLastRow() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getSheets()[0];
-  
+
   var lastRow = sheet.getLastRow();
   var lastColumn = sheet.getLastColumn();
   var lastCell = sheet.getRange(lastRow, lastColumn);
-  
+
   return sheet.getSheetValues(lastRow, 1, 1, lastColumn);
 }
 
 function getFirstRow() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = ss.getSheets()[0];
-  
+
   var lastColumn = sheet.getLastColumn();
   var lastCell = sheet.getRange(1, lastColumn);
-  
+
   return sheet.getSheetValues(1, 1, 1, lastColumn);
 }
 
 function createDataObject() {
   const labels = getFirstRow();
   const data = getLastRow();
-  
+
   var doc = {
     "event": "${this.eventId}",
   }
-  
+
   for (var i in labels[0])
   {
     doc[ labels[0][i] ] = data[0][i]
   }
-  
+
   return doc;
 }
 
@@ -99,9 +99,9 @@ function createDocument(data) {
   const email = "sheet-to-firestore@kcv-backoffice.iam.gserviceaccount.com";
   const key = "${data.content}";
   const projectId = "kcv-backoffice";
-  
+
   var firestore = FirestoreApp.getFirestore(email, key, projectId);
-  
+
   firestore.createDocument("registrations", data)
 }
       `;
