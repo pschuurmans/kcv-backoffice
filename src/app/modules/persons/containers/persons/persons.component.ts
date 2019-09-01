99c63e31dbf30ebf2908c20a917798c3c9838b70
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-09-01 20:38:48 +0200

    Update persons component with data retrieval

--- src/app/modules/persons/containers/persons/persons.component.ts
+++ src/app/modules/persons/containers/persons/persons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Person } from 'src/app/models/person';
 
 @Component({
   selector: 'app-persons',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persons.component.scss']
 })
 export class PersonsComponent implements OnInit {
+  readonly headerHeight = 50;
+  readonly rowHeight = 50;
 
-  constructor() { }
+  persons: Person[] = [];
+
+  columns = [
+    { name: 'Voornaam', prop: 'first_name' },
+    { name: 'Achternaam', prop: 'last_name' },
+    { name: 'Aangemaakt', prop: 'created_at', pipe: { transform: this.timestampPipe } }
+  ];
+
+  timestampPipe(value: any, ...args: any[]) {
+    const seconds = value.seconds * 1000; // Convert timestamp to date
+    return new Date(seconds).toLocaleString('nl-NL');
+  }
+
+  constructor(
+    private afs: AngularFirestore
+  ) { }
 
   ngOnInit() {
+    this.getPersons();
+  }
+
+  getPersons() {
+    this.afs.collection('persons').valueChanges()
+      .subscribe(
+        (data: Person[]) => this.persons = data,
+        error => console.log(error)
+      );
   }
 
 }
