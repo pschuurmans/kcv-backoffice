256e0e62bd3e155fbf90ff79302745ec85854e74
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-09-02 16:49:37 +0200

    Add timestamp pipe

---
+++ src/app/core/pipes/timestamp.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timestamp'
+})
+export class TimestampPipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]): any {
+    const seconds = value.seconds * 1000; // Convert timestamp to date
+    return new Date(seconds).toLocaleString('nl-NL');
+  }
+
+}
