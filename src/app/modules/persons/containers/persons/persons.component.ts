036649d26e44aa8be4b986a8366c49bb209ed6a3
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-09-02 16:50:18 +0200

    Update persons component

--- src/app/modules/persons/containers/persons/persons.component.ts
+++ src/app/modules/persons/containers/persons/persons.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Person } from 'src/app/models/person';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
@@ -11,33 +13,34 @@ export class PersonsComponent implements OnInit {
   readonly headerHeight = 50;
   readonly rowHeight = 50;
 
-  persons: Person[] = [];
-
-  columns = [
-    { name: 'Voornaam', prop: 'first_name' },
-    { name: 'Achternaam', prop: 'last_name' },
-    { name: 'Aangemaakt', prop: 'created_at', pipe: { transform: this.timestampPipe } }
-  ];
-
-  timestampPipe(value: any, ...args: any[]) {
-    const seconds = value.seconds * 1000; // Convert timestamp to date
-    return new Date(seconds).toLocaleString('nl-NL');
-  }
+  persons: any = [];
 
   constructor(
     private afs: AngularFirestore
   ) { }
 
   ngOnInit() {
-    this.getPersons();
+    this.getPersons().subscribe(
+      data => this.persons = data
+    );
   }
 
   getPersons() {
-    this.afs.collection('persons').valueChanges()
-      .subscribe(
-        (data: Person[]) => this.persons = data,
-        error => console.log(error)
+    return this.afs.collection('persons').snapshotChanges()
+      // https://github.com/angular/angularfire2/issues/1973
+      .pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        })
       );
+  }
+
+  deletePerson(id: number) {
+    this.afs.doc('persons/' + id).delete();
   }
 
 }
