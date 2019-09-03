766b9e70884f78fc1318541f164d03cbcc5dc652
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-09-03 20:10:35 +0200

    Change value person model to updated_at Fix person edit functionality Remove updated_at from table

--- src/app/modules/persons/components/person-edit/person-edit.component.ts
+++ src/app/modules/persons/components/person-edit/person-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import * as firebase from 'firebase';
+import { Person } from 'src/app/models/person';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-person-edit',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-edit.component.scss']
 })
 export class PersonEditComponent implements OnInit {
+  personId: string;
 
-  constructor() { }
+  private personDoc: AngularFirestoreDocument<Person>;
+  person: Observable<Person>;
+
+  personForm = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+  });
+
+  constructor(
+    private afs: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.personId = this.route.snapshot.paramMap.get('personId'); // Save url parameter in variable
+    afs.doc<Person>('persons/' + this.personId).valueChanges()
+      .subscribe(
+        data => this.personForm.patchValue(data)
+      );
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.afs.doc('persons/' + this.personId).update({
+      first_name: this.personForm.get('first_name').value,
+      last_name: this.personForm.get('last_name').value,
+      updated_at: firebase.firestore.FieldValue.serverTimestamp()
+    })
+      .then(
+        () => this.router.navigate(['/persons'])
+      );
+  }
 }
