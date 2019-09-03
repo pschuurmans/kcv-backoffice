725016f9728a5342b4142b7d2e6482ddd1e90ef7
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-09-03 15:47:49 +0200

    Fix production build and ignore linting error

--- src/app/store/reducers/auth.reducer.ts
+++ src/app/store/reducers/auth.reducer.ts
@@ -3,7 +3,13 @@ import { login, logout } from '../actions/auth.actions';
 
 export const initialState = false;
 
-export const authReducer = createReducer(initialState,
+// tslint:disable-next-line:variable-name
+const _authReducer = createReducer(initialState,
     on(login, state => true),
     on(logout, state => false)
 );
+
+
+export function authReducer(state, action) {
+    return _authReducer(state, action);
+}
