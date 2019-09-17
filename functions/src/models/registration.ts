319159c8aa3a0deeaaf49759572c9245d81cc10c
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-09-17 12:21:31 +0200

    Add registration model to functions

---
+++ functions/src/models/registration.ts
@@ -0,0 +1,28 @@
+import { firestore } from 'firebase/app';
+
+export interface Registration {
+    birthday: string | firestore.Timestamp;
+    city: string;
+    contact_preferences: string;
+    country: string;
+    email: string;
+    email2: string;
+    event_id: string;
+    first_aid: string;
+    first_name: string;
+    first_time: string;
+    house_nr: string;
+    job: string;
+    id?: string;
+    last_name: string;
+    mobile: string;
+    motivation: string;
+    notes: string;
+    participation: string;
+    phone: string;
+    phone2: string;
+    postal_code: string;
+    street: string;
+    timestamp: firestore.Timestamp;
+    track: string;
+}
