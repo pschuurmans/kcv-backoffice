c54c643daf0e3e3b7ddc2951b1353027f7784ba3
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-12-11 20:51:22 +0100

    Fix fix registrations

--- src/app/modules/registrations/components/registrations-fix/registrations-fix.component.ts
+++ src/app/modules/registrations/components/registrations-fix/registrations-fix.component.ts
@@ -7,32 +7,69 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./registrations-fix.component.scss']
 })
 export class RegistrationsFixComponent implements OnInit {
+  registrations = [];
 
-  constructor(
-    private afs: AngularFirestore
-  ) { }
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
-    this.afs.collection('registrations', ref => ref.where('event_id', '==', 'ht-2019')).snapshotChanges()
-    .subscribe(
-      data => {
-        data.forEach(element => {
-          const document: any = element.payload.doc.data();
-          if (document.rommmates !== undefined) {
-            this.afs.doc('registrations/' + element.payload.doc.id).update({
-              roommates: document.rommmates
-            });
-            console.log('Roommates fixed on document: ' + element.payload.doc.id);
-          }
-          if (document.postcal_code !== undefined) {
-            this.afs.doc('registrations/' + element.payload.doc.id).update({
-              postal_code: document.postcal_code
-            });
-            console.log('Postal_code fixed on document: ' + element.payload.doc.id);
-          }
-        });
-      }
-    );
+    this.afs
+      .collection('registrations', ref =>
+        ref.where('event_id', '==', 'ht-2019')
+      )
+      .get()
+      .toPromise()
+      .then(
+        data => {
+          console.log(data);
+          this.registrations = data.docs;
+          this.fixData();
+        }
+      );
   }
 
+  fixData() {
+    this.registrations.forEach(async element => {
+      const document: any = element.data();
+      if (document.rommmates !== undefined) {
+        await this.afs
+          .doc('registrations/' + element.id)
+          .update({
+            roommates: document.rommmates
+          })
+          .then(() =>
+            console.log(element.id + ' : Overwritten roommates')
+          );
+      }
+      if (document.postcal_code !== undefined) {
+        await this.afs
+          .doc('registrations/' + element.id)
+          .update({
+            postal_code: document.postcal_code
+          })
+          .then(() =>
+            console.log(element.id + ' : Overwritten postal_code')
+          );
+      }
+      // if (document.email !== undefined) {
+      //   await this.afs
+      //     .doc('registrations/' + element.id)
+      //     .update({
+      //       new_email: document.email
+      //     })
+      //     .then(() =>
+      //       console.log(element.id + ' : Overwritten new_email')
+      //     );
+      // }
+      // if (document.first_name !== undefined) {
+      //   await this.afs
+      //     .doc('registrations/' + element.id)
+      //     .update({
+      //       new_first_name: document.first_name
+      //     })
+      //     .then(() =>
+      //       console.log(element.id + ' : Overwritten new_first_name')
+      //     );
+      // }
+    });
+  }
 }
