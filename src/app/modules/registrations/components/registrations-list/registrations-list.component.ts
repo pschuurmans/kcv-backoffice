06724b0dac95ab533d396695f25d43887b254e0c
Pieter Schuurmans <schuurmans.pieter@gmail.com>
2019-12-17 19:25:50 +0100

    test

--- src/app/modules/registrations/components/registrations-list/registrations-list.component.ts
+++ src/app/modules/registrations/components/registrations-list/registrations-list.component.ts
@@ -75,22 +75,23 @@ export class RegistrationsListComponent implements OnInit {
   }
 
   export() {
+    console.log('export');
     // this.exportService.exportAsExcelFile(this.registrations, 'registrations');
-    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    const fileExtension = '.xlsx';
-    exportExcel(this.registrations, 'registrations');
+    // const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    // const fileExtension = '.xlsx';
+    // exportExcel(this.registrations, 'registrations');
 
-    function exportExcel(jsonData: any[], fileName: string): void {
-      const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
-      const wb: XLSX.WorkBook = { Sheets: { data: ws }, SheetNames: ['data'] };
-      const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-      saveExcelFile(excelBuffer, fileName);
-    }
+    // function exportExcel(jsonData: any[], fileName: string): void {
+    //   const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
+    //   const wb: XLSX.WorkBook = { Sheets: { data: ws }, SheetNames: ['data'] };
+    //   const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    //   saveExcelFile(excelBuffer, fileName);
+    // }
 
-    function saveExcelFile(buffer: any, fileName: string): void {
-      const data: Blob = new Blob([buffer], {type: this.fileType});
-      FileSaver.saveAs(data, fileName + this.fileExtension);
-    }
+    // function saveExcelFile(buffer: any, fileName: string): void {
+    //   const data: Blob = new Blob([buffer], {type: fileType});
+    //   FileSaver.saveAs(data, fileName + fileExtension);
+    // }
   }
 
 }
